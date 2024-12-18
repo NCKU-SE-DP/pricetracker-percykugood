d8b7191e05273786b901e966ab26dfd61772931c
percykugood <percyku71@gmail.com>
2024-12-19 00:46:52 +0800

    chore: Update Sentry DSN in configuration

--- backend/src/config.py
+++ backend/src/config.py
@@ -2,7 +2,7 @@ class UserConstants:
     user_max_length = 50
     password_max_length = 200
 class SentryConfig:
-    SENTRY_DSN = "https://4001ffe917ccb261aa0e0c34026dc343@o4505702629834752.ingest.us.sentry.io/4507694792704000"
+    SENTRY_DSN = "https://c6c709168e44059051c29975c9fd0c66@o4508454866780160.ingest.us.sentry.io/4508454877921280"
     TRACES_SAMPLE_RATE = 1.0
     PROFILES_SAMPLE_RATE = 1.0
 class AppConfig:
